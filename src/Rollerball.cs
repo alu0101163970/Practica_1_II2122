42846c3b51443f041fdfed82682852aedc0ae3f5
Francisco Jesus Mendes Gomez <alu0101163970@ull.edu.es>
2021-10-11 09:54:34 -0700

    scripts añadidos

---
+++ src/Rollerball.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rollerball : MonoBehaviour
+{
+    public GameObject objeto; 
+    int contador = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objeto = GameObject.FindWithTag("Rollerball");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Debug.Log($"Nombre del Objeto: {objeto.name}; Tag: {objeto.tag}; Contador del objeto: {contador++}"); 
+    }
+}
